c8e46530d253c20fe02b2b2d9c7676ad7e0d2060
aryteguh <41240575+aryteguh@users.noreply.github.com>
2020-07-28 22:54:48 +0700

    alhamdullilah

--- uas_provis/src/koneksi/koneksi.java
+++ uas_provis/src/koneksi/koneksi.java
@@ -17,23 +17,17 @@ import javax.swing.JOptionPane;
 public class koneksi {
     Connection koneksi;
     
-    public static Connection Koneksi() {
+   public static Connection Koneksi() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             
-            String db_connection = "mysql";
-            String db_host = "localhost";
-            String db_name = "db10118375rental";
-            String db_username = "root";
-            String db_password = "";
-            Connection koneksi = DriverManager.getConnection("jdbc:"+ db_connection +"://"+ db_host +"/"+ db_name +"", ""+db_username+"", ""+ db_password +"");
+            Connection koneksi = DriverManager.getConnection("jdbc:mysql://localhost/db10118375rental", "root", "");
             
-            return koneksi;
+          return koneksi;
         } catch(Exception e) {
             JOptionPane.showMessageDialog(null, e);
             
             return null;
         }
-
-    }
+   }
 }
